79d4b01660e2eddd7a4c634cced9c54441f9769e
Seb Insua <me@sebinsua.com>
2017-10-12 23:31:57 +0100

    chore: small correction to example

--- example/src/App.js
+++ example/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Counter />
+        <Counter id="1" />
       </div>
     )
   }
