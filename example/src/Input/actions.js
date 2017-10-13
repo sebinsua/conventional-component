b957e2f8d1b4177a6179c291041d3adb7f48bd12
Seb Insua <me@sebinsua.com>
2017-10-13 10:03:54 +0100

    chore: action namespace

--- example/src/Input/actions.js
+++ example/src/Input/actions.js
@@ -1,6 +1,8 @@
 import withActionIdentity from '../withActionIdentity'
 
-export const SET_FOCUS = 'input/SET_FOCUS'
+const prefix = 'Input'
+
+export const SET_FOCUS = `${prefix}/SET_FOCUS`
 
 export const setFocus = withActionIdentity(focus => ({
   type: SET_FOCUS,
