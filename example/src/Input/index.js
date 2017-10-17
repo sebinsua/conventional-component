91df6fdef27d4d76d5268d002ce8c07e74b823c3
Seb Insua <me@sebinsua.com>
2017-10-17 23:00:18 +0100

    chore: standard component without recompose

--- example/src/Input/index.js
+++ example/src/Input/index.js
@@ -1,35 +1,58 @@
-import { compose, withHandlers } from 'recompose'
+import React, { Component } from 'react'
 import receiveChildrenAsFunction from '../receiveChildrenAsFunction'
 import connectState from '../connectState'
+
 import reducer from './reducer'
 import * as actions from './actions'
-
 import InputDisplay from './InputDisplay'
 
-const enhance = compose(
-  connectState(reducer, actions),
-  withHandlers({
-    onBlur: props => event => {
+const enhance = connectState(reducer, actions)
+
+function create(DefaultTemplate) {
+  class Input extends Component {
+    onBlur = event => {
       event.preventDefault()
-      props.setFocus(false)
-    },
-    onChange: props => event => {
-      event.preventDefault()
-      props.setValue(event.target.value)
-    },
-    onFocus: props => event => {
-      event.preventDefault()
-      props.setFocus(true)
-    },
-    reset: props => () => {
-      props.setValue('')
+      return this.props.setFocus(false)
     }
-  })
-)
 
-const InputLogic = enhance(receiveChildrenAsFunction)
+    onChange = event => {
+      event.preventDefault()
+      return this.props.setValue(event.target.value)
+    }
 
-const Input = enhance(InputDisplay)
+    onFocus = event => {
+      event.preventDefault()
+      return this.props.setFocus(true)
+    }
+
+    reset = event => {
+      event.preventDefault()
+      return this.props.setValue('')
+    }
+
+    render() {
+      const templateProps = {
+        onBlur: this.onBlur,
+        onChange: this.onChange,
+        onFocus: this.onFocus,
+        reset: this.reset,
+        ...this.props
+      }
+
+      if (DefaultTemplate) {
+        return <DefaultTemplate {...templateProps} />
+      }
+
+      return receiveChildrenAsFunction(templateProps)
+    }
+  }
+
+  return Input
+}
+
+const InputLogic = enhance(create())
+
+const Input = enhance(create(InputDisplay))
 
 export { InputLogic, InputDisplay }
 export default Input
